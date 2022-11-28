a4ac9f758d948dc543c0772489117a87e7060707
SaeedZahran <saeedzahran@hotmail.com>
2022-11-28 16:59:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ import json
 import mne
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
 import matplotlib.image
+
+
+#workaround for -- _tkinter.TclError: invalid command name ".!canvas"
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 # Current path
 __location__ = os.path.realpath(
@@ -25,30 +32,19 @@ with open(__location__+'/config.json') as config_json:
 fname = config['epo']
 epochs = mne.read_epochs(fname)
 
-# == GET CONFIG VALUES ==
-reject_mag = config['reject_mag']
-reject_grad = config['reject_grad']
-reject_eeg = config['reject_eeg']
-reject_eog = config['reject_eog']
-
-flat_mag = config['flat_mag']
-flat_grad = config['flat_grad']
-flat_eeg = config['flat_eeg']
 
 
 
+reject_criteria_c = config['reject_criteria_c']
+reject_criteria = dict(x.split("=") for x in reject_criteria_c.split(";"))
+reject_criteria_n = dict(zip(reject_criteria.keys(), [float(value) for value in reject_criteria.values()]))
 
-reject_criteria = dict(mag=reject_mag,     # 3000 fT
-                       grad=reject_grad,    # 3000 fT/cm
-                       eeg=reject_eeg,       # 100 µV
-                       eog=reject_eog)       # 200 µV
-
-flat_criteria = dict(mag=flat_mag,          # 1 fT
-                     grad=flat_grad,         # 1 fT/cm
-                     eeg=flat_eeg)           # 1 µV
+flat_criteria_c = config['flat_criteria_c']
+flat_criteria = dict(x.split("=") for x in flat_criteria_c.split(";"))
+flat_criteria_n = dict(zip(reject_criteria.keys(), [float(value) for value in flat_criteria.values()]))
 
 
-epochs.drop_bad(reject=reject_criteria, flat=flat_criteria)
+epochs.drop_bad(reject=reject_criteria_n, flat=flat_criteria_n)
 
 
 # == SAVE FILE ==
